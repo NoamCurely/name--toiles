1aaac4d87271fec0b7a7c7d750ae5f8ac0a72af3
Dugrais julien <dugrais.julien@icloud.com>
2023-01-11 20:47:41 +0100

    mise en place pour que tu code lol

--- index.js
+++ index.js
@@ -1,52 +1,69 @@
-function code() {
-    $first = [
-        "Jupiter",
-        "Terre",
-    ];
 
-    $second = [
-        "Lune",
-        "Terre",
-        "Soleil",
-    ];
-
-    $third = [
-        "Terre",
-        "Mars",
-        "Mercure",
-    ];
-
-    $forth = [
-        "Pluton",
-        "Mercure",
-        "Terre",
-        "Mars",
-        "Calisto",
-    ];
-
-    $input = [
-        "value1",
-        "value2",
-        "value3",
-        "value4",
-        "value5"
-    ];
-
-    if ($first === $input) {
-        return "j6T4";
-    } else {
-        if ($second === $input) {
-            return "L3T4S5";
-        } else {
-            if ($third === $input) {
-                return "T4M3Me5";
-            } else {
-                if ($forth === $input) {
-                    return "P5M6T4Ma2C6";
-                } else {
-                    return "étapes invalid !";
-                }
-            }
-        }
-    }   
+function nameMission(array) {
+    //***************************************
+    //******* ALGORITHME ********************
+    //******* ici il faut coder *************
+    //***************************************
+    return 'ca serai facile';
 }
+
+
+//ne pas toucher après
+
+let test = () => {
+    let erreurs = 0;
+    let test = 1;
+    let result = [];
+    [
+        [
+            [
+                "Jupiter",
+                "Terre",
+            ],
+            "J6T4",
+        ],
+        [
+            [
+
+                "Lune",
+                "Terre",
+                "Soleil",
+            ],
+            "L3T4S5",
+        ],
+        [
+            [
+                "Terre",
+                "Mars",
+                "Mercure",
+            ],
+            "T4M3Me5",
+        ],
+        [
+            [
+                "Pluton",
+                "Mercure",
+                "Terre",
+                "Mars",
+                "Calisto",
+            ],
+            "P5M6T4Ma2C6",
+        ],
+    ].forEach(([input, expected]) => {
+        if (nameMission(input) === expected) {
+            result.push({ 'test n°': test, "resut": 'ok' });
+        } else {
+            result.push({ 'test n°': test, "resut": ('KO ' + nameMission(input) + ' est differant de ' + expected) });
+            erreurs++;
+        }
+        test++;
+    })
+    if (erreurs < 1) {
+        console.log("Bravo !!!!")
+    } else {
+        console.log("PAS BON")
+        console.table(result);
+    }
+}
+
+test();
