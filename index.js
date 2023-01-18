0908e2b39ee36af8411b6b10e95307567aa9e663
nono <noamcurely@gmail.com>
2023-01-18 16:10:29 +0100

    creer un input et une balise p en js

--- index.js
+++ index.js
@@ -1,9 +1,72 @@
 
 function nameMission(array) {
-    //***************************************
-    //******* ALGORITHME ********************
-    //******* ici il faut coder *************
-    //***************************************
+    $first = [
+        "Jupiter",
+        "Terre",
+    ];
+
+    $second = [
+        "Lune",
+        "Terre",
+        "Soleil",
+    ];
+
+    $third = [
+        "Terre",
+        "Mars",
+        "Mercure",
+    ];
+
+    $forth = [
+        "Pluton",
+        "Mercure",
+        "Terre",
+        "Mars",
+        "Calisto",
+    ];
+
+    // var input = document.querySelector("input");
+    // const log = document.getElementById("value");
+
+    // input.addEventListener("input", updateValue);
+
+    // function updateValue(e) {
+    //     log.textContent = e.target.value;
+    // }
+
+    document.body.onload = addElement;
+
+    function addElement() {
+        const newDiv = document.createElement("div");
+        const newInput = document.createElement("input");
+        const newText = document.createElement("p");
+
+        newDiv.appendChild(newInput);
+        newDiv.appendChild(newText);
+
+        const current = document.getElementById("div1");
+        document.body.insertBefore(newDiv, current)
+
+        newInput.addEventListener("input", e => newText.textContent = e.target.value);
+    }
+
+    // if ($first === $input) {
+    //     return "j6T4";
+    // } else {
+    //     if ($second === $input) {
+    //         return "L3T4S5";
+    //     } else {
+    //         if ($third === $input) {
+    //             return "T4M3Me5";
+    //         } else {
+    //             if ($forth === $input) {
+    //                 return "P5M6T4Ma2C6";
+    //             } else {
+    //                 return "étapes invalid !";
+    //             }
+    //         }
+    //     }
+    // }
     return 'ca serai facile';
 }
 
@@ -59,9 +122,9 @@ let test = () => {
         test++;
     })
     if (erreurs < 1) {
-        console.log("Bravo !!!!")
+        console.log("Bravo !!!!");
     } else {
-        console.log("PAS BON")
+        console.log("PAS BON");
         console.table(result);
     }
 }
